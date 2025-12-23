f8629bedd8e538358d1819b5a8212cd9805c1bc3
Colin Smith <colin76@gmail.com>
2025-12-23 14:15:03 -0500

    summary functions to create tabulations from the stages of the firm synthesis model, for use later in the model and in the dashboad

--- lib/scripts/firm_sim_summary.R
+++ lib/scripts/firm_sim_summary.R
@@ -1,11 +1,52 @@
 # Firm synthesis summary
-firm_synthesis_summary <- function(FirmsDomestic, producers, consumers, io, prefweights) {
+firm_synthesis_summary <- function(firm_sim_summary, FirmsDomestic, TAZEmployment, c_n2_empcats, 
+                                   FirmsDomesticBase, FirmSizeFactors, FirmsForeign, for_cons, for_prod, c_n6_n6io_sctg,
+                                   io_list, producers_list, producers, 
+                                   consumers_list, consumers, prefweights) {
 
-  # output summaries
+  ### Run the individual summary functions:
+  
+  if(SCENARIO_NAME == BASE_SCENARIO_BASE_NAME){
+    
+    # Base year firm size summary
+    firm_sim_summary$FirmSize <- firm_synthesis_summary_firm_size_base(FirmsDomestic, TAZEmployment, c_n2_empcats)
+  
+  } else {
+      
+    # Future year firm size summaries
+    firm_sim_summary$FirmSize <- firm_synthesis_summary_firm_size_future(FirmsDomesticBase, FirmsDomestic, TAZEmployment, FirmSizeFactors, c_n2_empcats)
+    firm_sim_summary$FirmSize3 <- firm_synthesis_summary_firm_size3(FirmsDomesticBase, FirmsDomestic, c_n2_empcats)
+    
+  }
+  
+  # Firms
+  firm_sim_summary$FirmsDomestic <- firm_synthesis_summary_domestic(FirmsDomestic)
+  firm_sim_summary$FirmsForeign <- firm_synthesis_summary_foreign(FirmsForeign, for_cons, for_prod, c_n6_n6io_sctg)
+  firm_sim_summary$SCTGCheck <- firm_synthesis_summary_sctg(FirmsDomestic, FirmsForeign)
+  
+  # IO Tables
+  firm_sim_summary$IOProdTradetype <- firm_synthesis_summary_io(io_list$io, c_n6_n6io_sctg)
+  firm_sim_summary$io <- io_list$io
+  firm_sim_summary$ioscaled <- io_list$ioscaled
+  firm_sim_summary$fromwhl <- io_list$fromwhl
+  firm_sim_summary$fromwhl_make <- io_list$fromwhl_make
+  firm_sim_summary$fromwhl_make_use <- io_list$fromwhl_make_use
+  firm_sim_summary$nonsctg <- io_list$nonsctg
+  
+  # Producers
+  firm_sim_summary$producers_all <- producers_list$producers_all
+  firm_sim_summary$producers <- firm_synthesis_summary_producers(producers)
+  
+  # Consumers
+  firm_sim_summary$consumers_incremental_summary <- consumers_list$consumers_incremental_summary
+  firm_sim_summary$consumers_summary <- firm_synthesis_summary_consumers(consumers, consumers_list$consumers_incremental_summary)
+  
+  ### Create high level output summaries:
+  
   sctgcat <- data.table(Commodity_SCTG = prefweights$Commodity_SCTG,
                         SCTG_Name = prefweights$Commodity_SCTG_desc)
 
-  # Sumamrize CBP, Producers and Consumers
+  # Summarize CBP, Producers and Consumers
   firms_sum <- list()
 
   # CBP
@@ -15,9 +56,9 @@ firm_synthesis_summary <- function(FirmsDomestic, producers, consumers, io, pref
                                             c("Commodity_SCTG", "SCTG_Name", "Establishments", "Employment"))
 
   # IO
-  firms_sum[["total_value"]] <- io[,.(ProVal = sum(ProVal))]$ProVal
-  firms_sum[["Industry_NAICS_Make"]] <- length(io[, unique(Industry_NAICS6_Make)])
-  firms_sum[["Industry_NAICS_Use"]] <- length(io[, unique(Industry_NAICS6_Use)])
+  firms_sum[["total_value"]] <- io_list$io[,.(ProVal = sum(TotVal))]$ProVal
+  firms_sum[["Industry_NAICS_Make"]] <- length(io_list$io[, unique(Industry_NAICS6_Make)])
+  firms_sum[["Industry_NAICS_Use"]] <- length(io_list$io[, unique(Industry_NAICS6_Use)])
 
   # Producers
   firms_sum[["producers"]] <- producers[,.N]
@@ -116,10 +157,215 @@ firm_synthesis_summary <- function(FirmsDomestic, producers, consumers, io, pref
 
   firms_sum[["matches_naics_sctg"]] <- data.frame(match_summary_naics_sctg) #so it prints all rows
 
-  #output
+  # Output text file/csv for the high level summaries
   capture.output(print(firms_sum),file=file.path(SCENARIO_OUTPUT_PATH,"firm_syn.txt" ))
   fwrite(match_summary_naics_sctg, file=file.path(SCENARIO_OUTPUT_PATH,"match_summary_naics_sctg.csv" ))
 
-  return(firms_sum)
+  # Add to the list of summaries and return
+  firm_sim_summary$firms_sum <- firms_sum
+  
+  return(firm_sim_summary)
 
+}
+
+firm_synthesis_summary_firm_size_base <- function(FirmsDomestic, TAZEmployment, c_n2_empcats) {
+  
+  # Base year firm size summary
+  FirmSize <- FirmsDomestic[, .(Firms.Model = .N, 
+                                Employees.Model = sum(Emp)), 
+                            keyby = EmpCatName]
+  FirmSize[, Size.Model := Employees.Model/Firms.Model]
+  FirmSize[TAZEmployment[, .(Employees.SE = sum(Employees.SE, na.rm = TRUE)), keyby = EmpCatName],
+           Employees.SE := i.Employees.SE, on = "EmpCatName"]
+  FirmSize[, Employees.Diff := Employees.Model - Employees.SE]
+  
+  # Labeling and column order
+  FirmSize[c_n2_empcats, EmpCatDesc := i.EmpCatDesc, on = "EmpCatName"]
+  setcolorder(FirmSize, c(names(FirmSize)[ncol(FirmSize)], names(FirmSize)[1:(ncol(FirmSize)-1)]))
+  
+  return(FirmSize)
+}
+
+firm_synthesis_summary_firm_size_future <- function(FirmsDomesticBase, FirmsDomestic, TAZEmployment, FirmSizeFactors, c_n2_empcats) {
+ 
+  # Base firms and employment
+  # Target firms and employment
+  # Summarize employment from synthesized firms by TAZ and EmpCatName
+  FirmSize <- FirmsDomesticBase[, .(Firms.Initial = .N, 
+                                    Employees.Initial = sum(Emp)), 
+                                keyby = EmpCatName]
+  
+  FirmSize[TAZEmployment[, .(Employees.SE = sum(Employees.SE, na.rm = TRUE)), keyby = EmpCatName],
+           Employees.SE := i.Employees.SE, on = "EmpCatName"]
+  FirmSize[, Size.Initial := Employees.Initial/Firms.Initial]
+  FirmSize[FirmSizeFactors[,.(EmpCatName, FirmSizeFactor)], 
+           FirmSizeFactor := i.FirmSizeFactor, on = "EmpCatName"]
+  FirmSize[, Size.Target := Size.Initial * FirmSizeFactor]
+  FirmSize[, Firms.Target := round(Employees.SE/Size.Target)]
+  
+  # Future (scaled) firms and employment
+  FirmSize[FirmsDomestic[, .(Firms.Updated = .N, 
+                             Employees.Updated = sum(Emp)), 
+                         keyby = EmpCatName],
+           c("Firms.Updated", "Employees.Updated") := .(i.Firms.Updated, i.Employees.Updated),
+           on = "EmpCatName"]
+  
+  # How well did the scaling do?
+  FirmSize[, Firms.Diff := Firms.Updated - Firms.Target]
+  FirmSize[, Employees.Diff := Employees.Updated - Employees.SE]
+  
+  # Check percentage error for firms, any outside reasonable tolerance?
+  FirmSize[, Firms.PctDiff := Firms.Diff/Firms.Target]
+  # check average size
+  FirmSize[, Size.Updated := Employees.Updated/Firms.Updated]
+  FirmSize[, Size.Diff := Size.Updated - Size.Target]
+  
+  # Labeling and column order
+  FirmSize[c_n2_empcats, EmpCatDesc := i.EmpCatDesc, on = "EmpCatName"]
+  setcolorder(FirmSize, c(names(FirmSize)[ncol(FirmSize)], names(FirmSize)[1:(ncol(FirmSize)-1)]))
+  
+  return(FirmSize)
+  
+}
+  
+firm_synthesis_summary_firm_size3 <- function(FirmsDomesticBase, FirmsDomestic, c_n2_empcats) {
+  
+  # Change in made up by 3 digit firms within 2 digit categories
+  FirmSize3 <- FirmsDomesticBase[, .(Firms.Initial = .N, 
+                                     Employees.Initial = sum(Emp)), 
+                                 keyby = .(EmpCatName, NAICS3 = floor(NAICS6/1000))]
+  FirmSize3[FirmsDomestic[, .(Firms.Updated = .N, 
+                              Employees.Updated = sum(Emp)), 
+                          keyby = .(EmpCatName, NAICS3 = floor(NAICS6/1000))],
+            c("Firms.Updated", "Employees.Updated") := .(i.Firms.Updated, i.Employees.Updated),
+            on = c("EmpCatName", "NAICS3")]
+  
+  FirmSize3[, PctEmp3.Initial := Employees.Initial/sum(Employees.Initial), by = EmpCatName]
+  FirmSize3[, PctEmp3.Updated := Employees.Updated/sum(Employees.Updated), by = EmpCatName]
+  FirmSize3[, PctEmp3.Diff := PctEmp3.Updated - PctEmp3.Initial]
+  FirmSize3[,.(EmpCatName, NAICS3, PctEmp3.Initial = round(PctEmp3.Initial,3), 
+               PctEmp3.Updated  = round(PctEmp3.Updated,3), 
+               PctEmp3.Diff = round(PctEmp3.Diff,3))]
+  
+  FirmSize3[c_n2_empcats, EmpCatDesc := i.EmpCatDesc, on = "EmpCatName"]
+  setcolorder(FirmSize3, c(names(FirmSize3)[ncol(FirmSize3)], names(FirmSize3)[1:(ncol(FirmSize3)-1)]))
+  
+  return(FirmSize3)
+  
+  
+}
+
+firm_synthesis_summary_domestic <- function(FirmsDomestic){
+  
+  # Summarize the domestic firms by NAICS make code
+  FirmsDomesticSummary <- FirmsDomestic[,.(Est = .N, Emp = sum(Emp)),
+                                        keyby = .(EmpCatName, EmpCatGroupedName,  
+                                                  Industry_NAICS6_Make, Commodity_SCTG,
+                                                  modelregion)]
+  FirmsDomesticSummary[, modelregion := factor(modelregion, labels = c("CMAP_Mod_Reg", "Rest_USA"))]
+  
+  FirmsDomesticSummary <- dcast.data.table(FirmsDomesticSummary,
+                                           EmpCatName + EmpCatGroupedName + Industry_NAICS6_Make + Commodity_SCTG ~ modelregion,
+                                           value.var = c("Est", "Emp"))
+  
+  FirmsDomesticSummary[, Est_Total := Est_CMAP_Mod_Reg + Est_Rest_USA]
+  FirmsDomesticSummary[, Emp_Total := Emp_CMAP_Mod_Reg + Emp_Rest_USA]
+  
+  return(FirmsDomesticSummary)
+  
+}
+
+firm_synthesis_summary_foreign <- function(FirmsForeign, for_cons, for_prod, c_n2_empcats){
+  
+  # Summarize the enumerated foreign firms with the trade data
+  FirmsForeignSummary <- FirmsForeign[,.(Flows = .N, ProVal = sum(ProVal)),
+                                      keyby = .(Industry = substr(Industry_NAICS6_Make,1,2), FirmType)]
+  FirmsForeignSummary <- add_totals(dcast.data.table(FirmsForeignSummary,
+                                          Industry ~ FirmType,
+                                          fun.aggregate = sum,
+                                          value.var = c("Flows", "ProVal")),
+                                    rowtotal = FALSE)
+                                    
+  for_cons_sum <- add_totals(for_cons[,.(USExpVal  = sum(USExpVal)/1e6), keyby = .(Industry = substr(Commodity_NAICS6,1,2))], rowtotal = FALSE)
+  for_prod_sum <- add_totals(for_prod[,.(USImpVal  = sum(USImpVal)/1e6), keyby = .(Industry = substr(Commodity_NAICS6,1,2))], rowtotal = FALSE)
+  FirmsForeignSummary[for_cons_sum, USExpVal := i.USExpVal, on = "Industry"]
+  FirmsForeignSummary[for_prod_sum, USImpVal := i.USImpVal, on = "Industry"]
+  FirmsForeignSummary[, ExportDiff := round(ProVal_ForeignConsumer - USExpVal,2)]
+  FirmsForeignSummary[, ImportDiff := round(ProVal_ForeignProducer - USImpVal,2)]
+  
+  return(FirmsForeignSummary)
+   
+}
+
+firm_synthesis_summary_sctg <- function(FirmsDomestic, FirmsForeign){
+  
+  # Check on foreign and domestic firms by SCTG
+  SCTGCheck <- merge(FirmsDomestic[, .(FirmsDomestic = .N), by = Commodity_SCTG][order(Commodity_SCTG)],
+                     FirmsForeign[, .(FirmsForeign = .N), by = Commodity_SCTG][order(Commodity_SCTG)],
+                     all = TRUE, 
+                     by = "Commodity_SCTG")
+  
+  SCTGCheck[is.na(FirmsDomestic), FirmsDomestic := 0]
+  SCTGCheck[is.na(FirmsForeign), FirmsForeign := 0]
+  
+  SCTGCheck[SCTGCodes[,.(Commodity_SCTG = SCTG, Label)], 
+            Commodity_SCTG_Label := i.Label,
+            on = "Commodity_SCTG"]
+  setcolorder(SCTGCheck, c(names(SCTGCheck)[ncol(SCTGCheck)], names(SCTGCheck)[1:(ncol(SCTGCheck)-1)]))
+  
+  SCTGCheck[, Commodity_SCTG := as.character(Commodity_SCTG)]
+  SCTGCheck <- add_totals(SCTGCheck, idcols = 2L)
+  
+  return(SCTGCheck)
+}
+
+firm_synthesis_summary_io <- function(io, c_n6_n6io_sctg){
+  
+  # Create a summary table from the io table
+  io_prod_tradetype <- io[,.(Domestic = sum(DomVal),
+                             Exports = sum(ExpVal),
+                             Imports = sum(ImpVal),
+                             DomProduction = sum(DomProdVal),
+                             DomConsumption = sum(DomConsVal),
+                             TotalValue = sum(TotVal)),
+                         keyby = .(Industry_NAICS6_Make)]
+  
+  # Add make labels
+  io_prod_tradetype[unique(c_n6_n6io_sctg[,.(Industry_NAICS6_Make, Industry_NAICS6_Make_desc)]),
+                    Industry_NAICS6_Make_Desc := i.Industry_NAICS6_Make_desc, 
+                    on = "Industry_NAICS6_Make"]
+  
+  setcolorder(io_prod_tradetype, c("Industry_NAICS6_Make_Desc", names(io_prod_tradetype)[1:ncol(io_prod_tradetype)-1]))
+  setorder(io_prod_tradetype, Industry_NAICS6_Make)
+  
+  return(io_prod_tradetype)
+  
+}
+
+firm_synthesis_summary_producers <- function(producers){
+  
+  # Create a summary table from the producers table
+  # Totals production by SCTG, model region, and type
+  prod_sum <- producers[,.(OutputCapacityTons = sum(OutputCapacityTons)),
+                        keyby = .(EmpCatName, EmpCatGroupedName, Commodity_SCTG, ProdType, FirmType, modelregion)]
+  
+  return(prod_sum)
+  
+}
+
+
+firm_synthesis_summary_consumers <- function(consumers, consumers_incremental){
+  
+  # Create a summary table from the consumers table 
+  # Add the summary of consumers_incremental to create a complete consumption summary 
+  # Totals consumption by SCTG, Industry, ConsType, Zone, and FAFZone
+  cons_sum <- consumers[, .(Requirements = sum(PurchaseAmountTons)), 
+                                    keyby = .(Commodity_SCTG, ConsType, 
+                                              EmpCatName, EmpCatGroupedName,
+                                              Zone, FAFZONE)][, ConsSegment := "Simulated in PMG"]
+  consumers_incremental[, ConsSegment := "Non-Simulated Consumption"]
+  cons_sum <- rbind(cons_sum, consumers_incremental)
+  
+  return(cons_sum)
+  
 }
