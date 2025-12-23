1934f79d076a5cdbd2336714db44ca3991d2eea6
Colin Smith <colin76@gmail.com>
2025-12-23 14:09:55 -0500

    updated foreign firms script

--- lib/scripts/firm_sim_enumerate_foreign.R
+++ lib/scripts/firm_sim_enumerate_foreign.R
@@ -1,71 +1,17 @@
 
-#Enumerate foreign firms and merge with correspondenses
+#Enumerate foreign firms and merge with correspondences
 firm_synthesis_enumerate_foreign <- function(for_prod, for_cons, c_n6_n6io_sctg){
 
-  # Both for_prod and for_cons include foreign public production/consumption value.
-  # Reallocate within each country to the remaining privately owned industies in proportion to their prod/cons value
-  # In the future, research commodity shares for public production and consumption and allocate in a more detailed way
-
-  # Foreign Production
-  for_prod_sum <- for_prod[, .(USImpVal = as.numeric(sum(USImpVal))), by = .(CBPZONE, FAFZONE)]
-  for_prod_sum_no_pub <- for_prod[!Commodity_NAICS6 %in% c(910000, 920000, 980000, 990000), .(USImpValNoPub = as.numeric(sum(USImpVal))), by = .(CBPZONE, FAFZONE)]
-  for_prod_sum <- merge(for_prod_sum,
-                        for_prod_sum_no_pub,
-                        by = c("CBPZONE", "FAFZONE"),
-                        all.x = TRUE)
-  for_prod_sum[is.na(USImpValNoPub), USImpValNoPub := 0]
-  for_prod_sum[, ProdScale := ifelse(USImpValNoPub > 0, USImpVal/USImpValNoPub, 0)]
-
-  # account for countries with no non-public production by reallocating within FAF ZONE so FAF ZONE production is conserved
-  for_prod_faf <- for_prod_sum[, .(USImpVal = sum(USImpVal)), by = FAFZONE]
-  for_prod_faf_no_pub <- for_prod_sum[USImpValNoPub > 0, .(USImpValNoPub = sum(USImpVal)), by = FAFZONE]
-  for_prod_faf <- merge(for_prod_faf,
-                        for_prod_faf_no_pub,
-                        by = "FAFZONE",
-                        all.x = TRUE)
-  for_prod_faf[, ProdScaleFAF := USImpVal/USImpValNoPub]
-
-  # do the scaling for foreign production
-  for_prod <- for_prod[!Commodity_NAICS6 %in% c(910000, 920000, 980000, 990000)]
-  for_prod[for_prod_sum, ProdScale := i.ProdScale, on = "CBPZONE"]
-  for_prod[for_prod_faf, ProdScaleFAF := i.ProdScaleFAF, on = "FAFZONE"]
-  for_prod[, USImpVal := USImpVal * ProdScale * ProdScaleFAF]
-
-  # Foreign Consumption
-  for_cons_sum <- for_cons[, .(USExpVal = as.numeric(sum(USExpVal))), by = .(CBPZONE, FAFZONE)]
-  for_cons_sum_no_pub <- for_cons[!Commodity_NAICS6 %in% c(910000, 920000, 980000, 990000), .(USExpValNoPub = as.numeric(sum(USExpVal))), by = .(CBPZONE, FAFZONE)]
-  for_cons_sum <- merge(for_cons_sum,
-                        for_cons_sum_no_pub,
-                        by = c("CBPZONE", "FAFZONE"),
-                        all.x = TRUE)
-  for_cons_sum[is.na(USExpValNoPub), USExpValNoPub := 0]
-  for_cons_sum[, ConsScale := ifelse(USExpValNoPub > 0, USExpVal/USExpValNoPub, 0)]
-
-  # account for counties with no non-public consumption by reallocating within FAF ZONE so FAF ZONE consumption is conserved
-  for_cons_faf <- for_cons_sum[, .(USExpVal = sum(USExpVal)), by = FAFZONE]
-  for_cons_faf_no_pub <- for_cons_sum[USExpValNoPub > 0, .(USExpValNoPub = sum(USExpVal)), by = FAFZONE]
-  for_cons_faf <- merge(for_cons_faf,
-                        for_cons_faf_no_pub,
-                        by = "FAFZONE",
-                        all.x = TRUE)
-  for_cons_faf[, ConsScaleFAF := USExpVal/USExpValNoPub]
-
-  # do the scaling for foreign consumption
-  for_cons <- for_cons[!Commodity_NAICS6 %in% c(910000, 920000, 980000, 990000)]
-  for_cons[for_cons_sum, ConsScale := i.ConsScale, on = "CBPZONE"]
-  for_cons[for_cons_faf, ConsScaleFAF := i.ConsScaleFAF, on = "FAFZONE"]
-  for_cons[, USExpVal := USExpVal * ConsScale * ConsScaleFAF]
-
-  # Enurerate foreign producers and consumers
+  # Enumerate foreign producers and consumers
   # Create one agent per country per commodity
   # Remove any records where the country and FAF zone are not known
-  Firms <- rbind(for_prod[!is.na(FAFZONE),.(Industry_NAICS6_CBP = Commodity_NAICS6,
-                             ProdVal = USImpVal/1000000,
+  Firms <- rbind(for_prod[!is.na(FAFZONE),.(Industry_NAICS6_CBP = as.integer(Commodity_NAICS6),
+                             ProVal = USImpVal/1000000,
                              CBPZONE,
                              FAFZONE,
                              FirmType = "ForeignProducer")],
-                 for_cons[!is.na(FAFZONE),.(Industry_NAICS6_CBP = Commodity_NAICS6,
-                             ProdVal = USExpVal/1000000,
+                 for_cons[!is.na(FAFZONE),.(Industry_NAICS6_CBP = as.integer(Commodity_NAICS6),
+                             ProVal = USExpVal/1000000,
                              CBPZONE,
                              FAFZONE,
                              FirmType = "ForeignConsumer")])
@@ -74,30 +20,37 @@ firm_synthesis_enumerate_foreign <- function(for_prod, for_cons, c_n6_n6io_sctg)
   Firms[c_n6_n6io_sctg,
         c("Industry_NAICS6_Make" , "Commodity_SCTG") := .(i.Industry_NAICS6_Make, i.Commodity_SCTG),
         on  = "Industry_NAICS6_CBP"]
-
-  # There are certain Industry_NAICS6_CBP codes that make multiple SCTG codes - split the Production Value
-  # to create one firm/country combination for each of the SCTG codes
-  Firms_211111 <- rbind(Firms[Industry_NAICS6_CBP == 211111][, c("Commodity_SCTG", "ProdVal") := .(16L, ProdVal * 0.45)],
-                        Firms[Industry_NAICS6_CBP == 211111][, c("Commodity_SCTG", "ProdVal") := .(19L, ProdVal * 0.55)])
-
-  Firms <- rbind(Firms[Industry_NAICS6_CBP != 211111],
-                 Firms_211111)
-
-  Firms_324110 <- rbind(Firms[Industry_NAICS6_CBP == 324110][, c("Commodity_SCTG", "ProdVal") := .(17L, ProdVal * 0.25)],
-                        Firms[Industry_NAICS6_CBP == 324110][, c("Commodity_SCTG", "ProdVal") := .(18L, ProdVal * 0.25)],
-                        Firms[Industry_NAICS6_CBP == 324110][, c("Commodity_SCTG", "ProdVal") := .(19L, ProdVal * 0.5)])
-
-  Firms <- rbind(Firms[Industry_NAICS6_CBP != 324110],
-                 Firms_324110)
-
-  # Create and save a diagnostics file -- firms that did not get a match in the IO NAICS/SCTG correspondence
-  # Then filter the firms with missing NAICS IO codes out (ideally there will be no firms removed)
-  fwrite(Firms[is.na(Industry_NAICS6_Make)], file = file.path(SCENARIO_OUTPUT_PATH, "FirmsForeign_No_Match_NAICSIO.csv"))
-  fwrite(Firms[is.na(Industry_NAICS6_Make), .(ProdVal = sum(ProdVal)), by = .(Industry_NAICS6_CBP, FirmType)], file = file.path(SCENARIO_OUTPUT_PATH, "FirmsForeign_No_Match_NAICSIO_Summary.csv"))
+  
+  # This function identifies producers who make 2+ commodities and
+  # simulates a specific commodity for them based on probability thresholds for multiple commodities
+  mult_n6cbp <- unique(c_n6_n6io_sctg[Commodity_SCTG > 0 & Proportion < 1, 
+                                      .(Industry_NAICS6_CBP, Commodity_SCTG, Proportion)])
+  
+  assign_mult_sctg <- function(n6cbp){
+    sample(mult_n6cbp[Industry_NAICS6_CBP == n6cbp]$Commodity_SCTG,
+           n6cbp_samp[Industry_NAICS6_CBP == n6cbp]$N,
+           replace = TRUE,
+           prob = mult_n6cbp[Industry_NAICS6_CBP == n6cbp]$Proportion)
+  }
+  
+  # Do this for both foreign producers and foreign consumers
+  # Foreign consumers are simply representative endpoints for a NAICS commodity that is exported
+  # and therefore need to be treated in the same way to match correctly with a single SCTG code
+  
+  n6cbp_samp <- Firms[Industry_NAICS6_CBP %in% unique(mult_n6cbp$Industry_NAICS6_CBP)][, .N, by = Industry_NAICS6_CBP]
+  
+  set.seed(BASE_SEED_VALUE)
+  
+  for (n6cbp in n6cbp_samp$Industry_NAICS6_CBP){
+    Firms[Industry_NAICS6_CBP == n6cbp, 
+          Commodity_SCTG := assign_mult_sctg(n6cbp)]
+  }
 
   # Summarize and remove any industries with unknown NAICS IO codes
-  Firms <- Firms[!is.na(Industry_NAICS6_Make),
-                 .(ProdVal = sum(ProdVal)),
+  # Also remove any flows that are coded as SCTG = 0, as these should 
+  # all be transportable commodities (i.e., actual goods being imported or exported)
+  Firms <- Firms[!is.na(Industry_NAICS6_Make) & Commodity_SCTG > 0,
+                 .(ProVal = sum(ProVal)),
                  by = .(Industry_NAICS6_Make, CBPZONE, FAFZONE, Commodity_SCTG, FirmType)]
 
   # Return the foreign firms table
