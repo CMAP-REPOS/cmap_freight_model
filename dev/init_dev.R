cbd1bbefea90dae479880cec68b6e2331dccfec8
Colin Smith <colin76@gmail.com>
2026-01-21 12:57:01 -0500

    old base year reference files used in base year common data files update

--- dev/init_dev.R
+++ dev/init_dev.R
@@ -14,7 +14,8 @@ if(SYSTEM_DEV_PKGS != "") loadPackages(SYSTEM_DEV_PKGS, SYSTEM_PKGS_PATH)
 ### Define development paths -----------------------------------------------------------------
 
 # Define standard file paths for dev folder struture
-SYSTEM_DEV_DATA_PATH                   <- file.path(SYSTEM_DEV_PATH, "Data_Processed")
+SYSTEM_DEV_DATA_PATH                   <- file.path(SYSTEM_DEV_PATH, "BaseData")
+SYSTEM_DEV_FUTURE_PATH                 <- file.path(SYSTEM_DEV_PATH, "FutureScenarios")
 SYSTEM_DEV_MODEL_PATH                  <- file.path(SYSTEM_DEV_DATA_PATH, "Models")
 SYSTEM_DEV_ESTIMATION_PATH             <- file.path(SYSTEM_DEV_PATH, "Estimation")
 SYSTEM_DEV_TESTING_PATH                <- file.path(SYSTEM_DEV_PATH, "Testing")
